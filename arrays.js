f016be5222973064a54dd879f8e34ee54f4dd96c
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-27 12:33:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, 'foo') {
+function addElementToBeginningOfArray(array, element) {
   // array.unshift(element);
   
   var secondArray = [element, ...array];
