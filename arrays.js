aeb2cc5c6db7107fcd9ac85be34bbfe233a4400e
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-27 12:28:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -37,3 +37,7 @@ function removeElementFromBeginningOfArray(array) {
   var newArray = array.slice(1);
   return newArray;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
