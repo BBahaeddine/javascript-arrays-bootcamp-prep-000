4a18f38c0b9596803e1acb61abc6f7d0b6720c90
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-02-27 12:18:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 }
 
-function addElementToEndOfArray() {
+function addElementToEndOfArray(arra, element) {
   
 }
 
